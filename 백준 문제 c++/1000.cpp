4a81fc490143214eae475d4567d46e10f9705c0c
Doe <dohyung97022@gmail.com>
2020-11-03 22:14:01 +0900

    진인사대천명

---
+++ 백준 문제 c++/1000.cpp
@@ -0,0 +1,9 @@
+#include<iostream>;
+using namespace std;
+int main(){
+    bool isMale = true;
+    bool isTall = true;
+    if (isMale&&isTall){
+        cout<<"you are a tall male."<<endl;
+    }
+}
