f9e3695fe84fa0e1aa16f4620003b07216ab50da
Doe <dohyung97022@gmail.com>
2020-12-27 13:11:25 +0900

    이사람은 check의 int matrix에 거리를 직접 입력하였다. 이것도 방법

--- note.cpp
+++ note.cpp
@@ -1,74 +1,64 @@
-#include <iostream>
-#include <string>
-using namespace std;
-class Tree
-{
-    string data;
-    Tree *left, *right;
+//BJ2178
 
-public:
-    Tree()
-    {
-        data = "";
-        left = NULL;
-        right = NULL;
-    }
-    void setData(char data) { this->data = data; }
-    void setLeft(Tree *left) { this->left = left; }
-    void setRight(Tree *right) { this->right = right; }
-    void static preorder(Tree *root)
-    {
-        if (root)
-        {
-            cout << root->data;
-            preorder(root->left);
-            preorder(root->right);
-        }
-    }
-    void static inorder(Tree *root)
-    {
-        if (root)
-        {
-            inorder(root->left);
-            cout << root->data;
-            inorder(root->right);
-        }
-    }
-    void static postorder(Tree *root)
-    {
-        if (root)
-        {
-            postorder(root->left);
-            postorder(root->right);
-            cout << root->data;
-        }
-    }
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int map[101][101] = {
+    0,
 };
+int check[101][101] = {
+    0,
+};
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+int n, m;
+
+void BFS(int x, int y)
+{
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    check[x][y] = 1;
+
+    while (!q.empty())
+    {
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (0 <= nx && nx < n && 0 <= ny && ny < m)
+            {
+                if (check[nx][ny] == 0 && map[nx][ny] == 1)
+                {
+                    q.push(make_pair(nx, ny));
+                    check[nx][ny] = check[x][y] + 1;
+                }
+                else if (check[nx][ny] == 0)
+                    check[nx][ny] = -1;
+            }
+        }
+    }
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    Tree *tree = new Tree[n];
-    for (int i = 0; i < n; ++i)
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++)
     {
-        char data, left, right;
-        cin >> data >> left >> right;
-        if (data != '.')
-            tree[(int)(data - 'A')].setData(data);
-        if (left != '.')
-            tree[(int)(data - 'A')].setLeft(&tree[(int)(left - 'A')]);
-        else
-            tree[(int)(data - 'A')].setLeft(NULL);
-        if (right != '.')
-            tree[(int)(data - 'A')].setRight(&tree[(int)(right - 'A')]);
-        else
-            tree[(int)(data - 'A')].setRight(NULL);
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%1d", &map[i][j]);
+        }
     }
-    Tree *root = &tree[0];
-    Tree::preorder(root);
-    cout << endl;
-    Tree::inorder(root);
-    cout << endl;
-    Tree::postorder(root);
-    cout << endl;
+
+    BFS(0, 0);
+
+    printf("%d\n", check[n - 1][m - 1]);
 }
