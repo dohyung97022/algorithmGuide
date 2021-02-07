5b6a0fb00bbf89bcb6db9eda894056da8a31ff45
Doe <dohyung97022@gmail.com>
2021-02-07 11:45:27 +0900

    최소 힙의 priority que로 구현

---
+++ 백준 문제 c++/1927.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int> pQue;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+    int inputLen, input;
+    cin >> inputLen;
+    while (inputLen--)
+    {
+        cin >> input;
+        if (input == 0)
+        {
+            if (pQue.size())
+            {
+                cout << -pQue.top() << "\n";
+                pQue.pop();
+            }
+            else
+                cout << "0\n";
+        }
+        else
+            pQue.push(-input);
+    }
+}
